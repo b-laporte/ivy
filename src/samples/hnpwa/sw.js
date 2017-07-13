053998406f11b091116ad7f70133a8e3907ec66e
Marc Laval <mlaval@amadeus.com>
2017-07-13 18:42:18 +0200

    sample: hnpwa

---
+++ src/samples/hnpwa/sw.js
@@ -0,0 +1,59 @@
+var CACHE_NAME = 'iv-hnpwa-cache';
+var urlsToCache = [
+  './',
+  './hnpwa.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response && !event.request.url.startsWith('https://node-hnapi.herokuapp.com/')) {
+          return response;
+        }
+
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest)
+          .catch(() => {
+            return caches.match(event.request);
+          })
+          .then((response) => {
+            // Check if we received a valid response
+            if(!response || response.status !== 200) {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});

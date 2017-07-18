47b022dc6b864d648af64f2279c7f5d025e231d2
Marc Laval <mlaval@amadeus.com>
2017-07-18 15:33:53 +0200

    sample: js-framework-benchmark

--- rollup.samples.config.js
+++ rollup.samples.config.js
@@ -2,6 +2,7 @@ import largeTableConfig from './src/samples/benchmarks/largetable/rollup.config.
 import treeConfig from './src/samples/benchmarks/tree/rollup.config.js';
 import flexConfig from './src/samples/flex/rollup.config.js';
 import hnpwaConfig from './src/samples/hnpwa/rollup.config.js';
+import jfbConfig from './src/samples/js-framework-benchmark/rollup.config';
 import svgConfig from './src/samples/svg/rollup.config.js';
 import todoMVCConfig from './src/samples/todomvc/rollup.config';
 
@@ -9,7 +10,8 @@ export default [
   largeTableConfig,
   treeConfig,
   flexConfig,
-  todoMVCConfig,
   hnpwaConfig,
-  svgConfig
+  jfbConfig,
+  svgConfig,
+  todoMVCConfig
 ];

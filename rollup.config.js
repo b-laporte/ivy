5f8e37598762daf4ffd8fd7719ecf6f32320db50
b-laporte <blaporte@amadeus.com>
2020-04-23 15:21:47 +0200

    xjs migration: pre-processors & plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,59 @@
 import typescript from 'rollup-plugin-typescript2';
+import json from 'rollup-plugin-json';
 
-export default [
-// {
-//     input: 'src/xtr/extract.ts',
-//     output: {
-//         file: 'bin/rollup-plugin-ivy-extract.js',
-//         format: 'es'
-//     },
-//     external: [
-//         'typescript',
-//         'fs',
-//         'path',
-//         'vscode-textmate'
-//     ],
-//     plugins: [
-//         typescript({
-//             tsconfig: "tsconfig.rollup.json"
-//         })
-//     ]
-// },
-{
+export default [{
+    input: 'src/pre-processors/ts.ts',
+    output: {
+        file: 'bin/rollup-plugin-ivy-ts.js',
+        format: 'es'
+    },
+    external: [
+        'typescript',
+        'fs',
+        'path',
+        'vscode-textmate'
+    ],
+    plugins: [
+        json(),
+        typescript({
+            tsconfig: "tsconfig.rollup.json"
+        })
+    ]
+}, {
+    input: 'src/pre-processors/extract.ts',
+    output: {
+        file: 'bin/rollup-plugin-ivy-extract.js',
+        format: 'es'
+    },
+    external: [
+        'typescript',
+        'fs',
+        'path',
+        'vscode-textmate'
+    ],
+    plugins: [
+        json(),
+        typescript({
+            tsconfig: "tsconfig.rollup.json"
+        })
+    ]
+}, {
+    input: 'src/pre-processors/md.ts',
+    output: {
+        file: 'bin/rollup-plugin-ivy-md.js',
+        format: 'es'
+    },
+    external: [
+        'typescript',
+        'marked'
+    ],
+    plugins: [
+        json(),
+        typescript({
+            tsconfig: "tsconfig.rollup.json"
+        })
+    ]
+},{
     input: 'src/iv/rollup/rollup-plugin-ivy.ts',
     output: {
         file: 'bin/rollup-plugin-ivy.js',
